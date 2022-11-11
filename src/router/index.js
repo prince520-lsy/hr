bce94a55bd1520656c8e3be6c07522f86032a656
jinjinliang <3122702689@qq.com>
2022-11-11 08:55:59 +0800

    project-finished

--- src/router/index.js
+++ src/router/index.js
@@ -110,8 +110,8 @@ export const asyncRoutes = [
 const createRouter = () => new Router({
   // mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
-  // routes: [...constantRoutes, ...asyncRoutes]
-  routes: [...constantRoutes]
+  routes: [...constantRoutes, ...asyncRoutes]
+  // routes: [...constantRoutes]
 })
 
 const router = createRouter()
